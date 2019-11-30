90829571ebb9642fd26f98ec59cf45fd62a8284a
Alberto Vila <alberto.vilatena@gmail.com>
2019-12-01 00:28:15 +0200

    Basic JWT authentication

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restplus import Api, Resource, fields
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
 from exception.restrictions import LikeOwnItemException
 
 import services.product_service as Products
 import services.users_service as Users
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
 api = Api(app, version='1.0')
 
 login = api.namespace('login', description="Logging in")
@@ -19,10 +27,16 @@ login_fields = api.model('Resource', {
 @login.expect(login_fields)
 class Login(Resource):
     def post(self):
-        return {
-            "status": "Logging in"
-        }
+        user = Users.authenticate(api.payload)
 
+        if user:
+            access_token = create_access_token(identity=user['id'])
+            return {
+                "access_token": access_token
+            }
+        else:
+            return 'Email or password did not match any registered user', 401
+        
 
 header_parser = api.parser()
 header_parser.add_argument('Authorization', location='headers')
@@ -40,28 +54,42 @@ class Feed(Resource):
     sorting_parser.add_argument('order', help='Order to sort by (ASC or DESC)', choices=('ASC', 'DESC'),)
 
     @products.expect(filters_parser, sorting_parser)
+    @jwt_required
     def get(self):
+        user_id = get_jwt_identity();
+        user = Users.get(user_id)
+
         filters = self.filters_parser.parse_args()
         sorting = self.sorting_parser.parse_args()
 
-        return Products.feed(Users.get(1), filters, sorting)
+        return Products.feed(user, filters, sorting)
 
 @products.route("/<int:id>")
 @products.expect(header_parser)
 class Product(Resource):
+
+    @jwt_required
     def get(self, id):
+        user_id = get_jwt_identity();
+        user = Users.get(user_id)
+
         try:
-            return Products.get(Users.get(1), id)
+            return Products.get(user, id)
         except StopIteration:
             return 'Not Found', 404
 
 @products.route("/<int:id>/like")
 @products.expect(header_parser)
 class Like(Resource):
+    
+    @jwt_required
     def post(self, id):
+        user_id = get_jwt_identity();
+        user = Users.get(user_id)
+
         try:
-            product = Products.get(Users.get(1), id)
-            return Products.like(Users.get(1), product)
+            product = Products.get(user, id)
+            return Products.like(user, product)
         except LikeOwnItemException:
             return 'Cannot like own items', 400
         except StopIteration:
--- services/users_service.py
+++ services/users_service.py
@@ -1,7 +1,16 @@
 import json
+from werkzeug.security import safe_str_cmp
 
 with open('resources/users.json') as f:
     users = json.load(f)
 
 def get(id):
     return next(user for user in users if user['id'] == id)
+
+def get_by_email(email):
+    return next(user for user in users if user['email'] == email)
+
+def authenticate(payload):
+    user = get_by_email(payload['email'])
+    if user and safe_str_cmp(user['password'], payload['password']):
+        return user
